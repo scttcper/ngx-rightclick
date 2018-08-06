54433a28a3025daa57cf1d7e3c0046fb62e4d97d
Scott Cooper <scttcper@gmail.com>
2018-08-05 19:11:44 -0700

    add license

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,1 +1,6 @@
 export * from './context-menu.module';
+export * from './context-menu-injector';
+export * from './menu.component';
+export * from './context-submenu-trigger.directive';
+export * from './context-menu-trigger.directive';
+export * from './context-menu.service';
