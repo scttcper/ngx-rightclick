afcc6a3138ac5a430e83494088119c8de0177940
Matan Sar-Shalom <smatan84@gmail.com>
2019-10-17 09:00:41 +0300

    feat(menuTriggerDirective): add beforeOpen event (#35)
    * feat(menuTriggerDirective): add preventOpen event
    
    * prettier
    
    * prettier

--- src/app/multiple/multiple.component.ts
+++ src/app/multiple/multiple.component.ts
@@ -5,15 +5,19 @@ import { MultipleMenuComponent } from './multiple-menu.component';
 @Component({
   selector: 'simple-component',
   template: `
-  <div class="mb-3 d-flex flex-row justify-content-around bd-highlight">
-    <div *ngFor="let item of items"
-      class="p-3 p-md-4 p-lg-5 mr-1 bd-highlight bg-light border rounded target"
-      [contextMenuTrigger]="menu" [menuContext]="item"
-      (menuAction)="handleClose($event)">
-      {{ item.name }} ({{ item.count }})
+    <div class="mb-3 d-flex flex-row justify-content-around bd-highlight">
+      <div
+        *ngFor="let item of items"
+        class="p-3 p-md-4 p-lg-5 mr-1 bd-highlight bg-light border rounded target"
+        [contextMenuTrigger]="menu"
+        [menuContext]="item"
+        (beforeOpen)="onBeforeOpen($event)"
+        (menuAction)="handleClose($event)"
+      >
+        {{ item.name }} ({{ item.count }})
+      </div>
     </div>
-  </div>
-  <div *ngFor="let msg of messages">{{ msg }}</div>
+    <div *ngFor="let msg of messages">{{ msg }}</div>
   `,
 })
 export class MultipleComponent {
@@ -27,5 +31,12 @@ export class MultipleComponent {
   handleClose(msg: string) {
     this.messages.unshift(msg);
     this.messages.splice(10, this.messages.length);
+  }
+
+  onBeforeOpen($event: any) {
+    const { menuContext, preventOpen } = $event;
+    if (menuContext.name === 'orange') {
+      preventOpen();
+    }
   }
 }
--- src/lib/context-menu-trigger.directive.ts
+++ src/lib/context-menu-trigger.directive.ts
@@ -7,7 +7,6 @@ import {
   OnDestroy,
   OnInit,
 } from '@angular/core';
-
 import { Subscription } from 'rxjs';
 
 import { ContextMenuService, ActiveContextMenu } from './context-menu.service';
@@ -22,6 +21,8 @@ export class ContextMenuTriggerDirective implements OnDestroy, OnInit {
   @Input() holdToDisplay = 1000;
   @Output() menuAction = new EventEmitter<any>();
   @Output() menuClose = new EventEmitter<void>();
+  @Output() beforeOpen = new EventEmitter<any>();
+
   menu: ActiveContextMenu;
   visible = false;
   private mouseDownTimeoutId: any;
@@ -29,6 +30,19 @@ export class ContextMenuTriggerDirective implements OnDestroy, OnInit {
 
   @HostListener('contextmenu', ['$event'])
   handleMenu($event: MouseEvent) {
+    let preventOpen = false;
+    this.beforeOpen.emit({
+      menuContext: this.menuContext,
+      event: $event,
+      preventOpen: () => {
+        preventOpen = true;
+      },
+    });
+
+    if (preventOpen) {
+      return;
+    }
+
     $event.preventDefault();
     this.menu = this.contextMenuService.show(
       $event,
@@ -62,8 +76,9 @@ export class ContextMenuTriggerDirective implements OnDestroy, OnInit {
   constructor(private contextMenuService: ContextMenuService) {}
 
   ngOnInit() {
-    this.sub = this.menuClose.subscribe(() => this.visible = false);
+    this.sub = this.menuClose.subscribe(() => (this.visible = false));
   }
+
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
