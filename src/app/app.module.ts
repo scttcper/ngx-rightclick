6ebfa80d546a037de4b0deeeaae2278afdf6ecb3
Kevin <1-0-1@users.noreply.github.com>
2023-01-17 09:35:35 -0800

    Updated 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,7 @@ const routes: Routes = [
     ContextMenuModule,
     BrowserAnimationsModule,
     GhButtonModule,
-    RouterModule.forRoot(routes, { useHash: true }),
+    RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' }),
   ],
   bootstrap: [AppComponent],
   entryComponents: [
